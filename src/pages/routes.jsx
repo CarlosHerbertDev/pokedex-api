d5aff007503dc103945154c080d1dec3ebbd2e50
Carlos Herbert <hrbrtvieira@gmail.com>
2025-02-11 19:30:37 -0300

    concertando rotas

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -2,15 +2,15 @@ import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom"
 import { RenderListPokemons } from "./renderListPokemons"
 import { RenderDetailsPokemons } from "./renderDetailsPokemons"
 
-
+HashRouter
 
 const AppRoutes = () => (
-    <HashRouter>
+    <BrowserRouter>
         <Routes>
             <Route exact path='/' element={ <RenderListPokemons /> } />
             <Route exact path='/:id' element={ <RenderDetailsPokemons /> } />
         </Routes>
-    </HashRouter>
+    </BrowserRouter>
 )
 
 export { AppRoutes }
